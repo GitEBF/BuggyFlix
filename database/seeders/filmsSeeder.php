d7b994d3f0f8280980ee91e5708a4bfa743a1840
tijaycegep <barackobama.amidelaforet@gmail.com>
2023-10-23 13:52:57 -0400

    97yi8ukhj,mn

--- database/seeders/filmsSeeder.php
+++ database/seeders/filmsSeeder.php
@@ -201,7 +201,7 @@ class FilmsSeeder extends Seeder
                 'type' =>'Film',
                 'genre' => 'Horreur',
                 'brand' => 'buggy cheese',
-                'pochette' => 'https://imgur.com/a/g3L0Rjf',
+                'pochette' => 'https://i.imgur.com/HjTTpOX.png',
                 'duree' => '2h36',
                 'annee' => '2023',
                 'date' => date('2023-04-30'),
