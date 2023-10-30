8de53e20a2de8679c131bda5d54d3fc0968ce740
chumnon <lamothear02@gmail.com>
2023-10-30 14:17:19 -0400

    page film

--- resources/views/buggyflix/show.blade.php
+++ resources/views/buggyflix/show.blade.php
@@ -1,12 +1,38 @@
 @extends("layouts.app")
 
+<link rel="stylesheet" href="{{asset('css/pageFilm.css')}}">
 @section("contenu")
-<section class="main-container">
-    <div>
-<h1>Page du film {{$film->titre}}</h1>
+<section class="main-container" >
+    <div >
 @if (isset($film))
-    <li>{{ $film->titre}} </li>
-    <li>{{$film->resume}} </li>
+    <div class="pageFilm">
+        <div class="infoFilm">
+            <h1 class="titreFilm">{{$film->titre}} </h1>
+            <a>{{$film->cote}} </a>
+            <a>{{$film->date}} </a>
+            <a>{{$film->duree}} </a>
+            </br>
+            <a>{{$film->rating}} </a>
+            <a>{{$film->type}} </a>
+            <a>liste des genres</a>
+            </br>
+            <a>{{$film->resume}} </a>
+            </br>
+            <a>{{$film->langue}} </a>
+            <a>{{$film->subtitle}} </a>
+            </br>
+            <a>{{$film->brand}} </a>
+            </br>
+            <a>Acteur: liste des acteurs</a>
+            </br>
+            <a>Réalisateur: liste des réalisateur</a>
+            </br>
+            <a>Producteur: liste des producteur</a>
+        </div>
+        <div class="imgFilm">
+            <img class="pochetteFilm" src="{{$film->pochette}}">
+        </div>
+    </div>
 @else
     <p>Le film n'existe pas</p>
 @endif
