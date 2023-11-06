35e8636b2e777f10b2049e07afcbed239cf22ca0
tijaycegep <barackobama.amidelaforet@gmail.com>
2023-11-06 13:23:45 -0500

    ajouter personne

--- app/Http/Controllers/PersonsController.php
+++ app/Http/Controllers/PersonsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Acteur;
 use Illuminate\Http\Request;
 use App\Models\Person;
+use Illuminate\Support\Facades\Log;
 
 class PersonsController extends Controller
 {
@@ -32,11 +33,12 @@ class PersonsController extends Controller
     public function store(Request $request)
     {
         try{
-            $persons=new Acteur($request->all());
+            $persons=new Person($request->all());
             $persons->save();
             }
             
             catch(\Throwable$e){
+                Log::debug($e);
             }
             return redirect()->route('buggyflix.index');
             
