1d247285e2d77c114b13757de7d3a1db5fe70c12
chumnon <lamothear02@gmail.com>
2023-11-09 16:52:51 -0500

    douleur

--- resources/views/buggyflix/create/acteur.blade.php
+++ resources/views/buggyflix/create/acteur.blade.php
@@ -3,15 +3,7 @@
 @section("contenu")
 <link rel="stylesheet" href="{{ asset('css/form.css') }}">
 <section class="main-container"> <!-- component -->
-@if(isset($errors) && $errors->any())
-    
-    <div class="alert alert-danger">
-        @foreach($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
-    </div>
 
-@endif
 
 
 <section class="max-w-4xl p-6 mx-auto bgFlix rounded-md shadow-md mt-20">
@@ -56,6 +48,17 @@
         </button>
     </div>
 </form>
+
+@if(isset($errors) && $errors->any())
+    
+    <div class="alert alert-danger">
+        @foreach($errors->all() as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+    </div>
+
+@endif
+
 </div>
 <div>
     </section>
