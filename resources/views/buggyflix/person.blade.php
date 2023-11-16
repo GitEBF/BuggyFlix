5d6c7c52d572469606e821a07b5d7a91f116210a
chumnon <lamothear02@gmail.com>
2023-11-16 15:51:18 -0500

    page acteur nono

--- resources/views/buggyflix/person.blade.php
+++ resources/views/buggyflix/person.blade.php
@@ -1,10 +1,12 @@
 @extends("layouts.app")
 
 @section("contenu")
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <section class="main-container flex justify-center">
+@auth
 <div class="location" id="home">
-<a class="top-right-link" href="{{ route('buggyflix.create.person')}}"><i class="fa fa-plus"></i></a>
+  <a class="top-right-link" href="{{ route('buggyflix.create.person')}}"><i class="fa fa-plus"></i></a>
       <h1 id="home">Les gens toutes biggie</h1>
       <div class="box">
       @if (count($persons))
@@ -16,6 +18,11 @@
         @endif
       </div>
   </div>
+  @else
+  <div class="main-container location" id="home">
+   <a class="location" href="{{ route('UsagersController.login')}}"><p>Veuillez vous connecter</p></a>
+</div>
+  @endauth
 </section>
 
 <style>
@@ -29,6 +36,7 @@
     border-radius: 5px;
   }
 </style>
+
 
 @endsection
 
