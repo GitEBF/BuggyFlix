12fc513875856e310aefca7e9a0a94853cf4fbc3
chumnon <lamothear02@gmail.com>
2023-12-04 14:53:31 -0500

    pain

--- resources/views/buggyflix/index.blade.php
+++ resources/views/buggyflix/index.blade.php
@@ -6,65 +6,58 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.rawgit.com/kamranahmedse/jquery-toast-plugin/master/dist/jquery.toast.min.js"></script>
 <!-- MAIN CONTAINER -->
-<section class="main-container">
-
+<section class="main-container flex justify-center">
   @auth
-  @role(1,2)
-  <div class="location" id="home"> <h1 id="home">Nouveau sur Buggyflix</h1> <div class="box">
-    @if (count($films))
-    @foreach ($filmsbyDate->take(6) as $film)
-    <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}"></a>
-    @endforeach
+    <div class="location" id="home">
+      @role(1,2)
+        <h1 id="home">Nouveau sur Buggyflix</h1> 
+        <div class="box">
+          @if (count($films))
+            @foreach ($filmsbyDate->take(6) as $film)
+              <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}" class="h-48 w-48 mx-auto"></a>
+            @endforeach
+          @else
+            <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/t1.PNG?raw=true" alt=""></a>
+          @endif
+        </div>
+      @endrole
 
-    @else
-    <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/t1.PNG?raw=true" alt=""></a>
-    @endif
+      @role(1,3)
+        <h1 id="home">Film pour enfant</h1> 
+        <div class="box">
+          @if (count($films))
+            @foreach ($filmsPG->take(6) as $film)
+              <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}" class="h-50 w-50 mx-auto"></a>
+            @endforeach
+          @else
+            <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/t1.PNG?raw=true" alt=""></a>
+          @endif
+        </div>
+      @endrole
+
+      @role(1,2)
+        <h1 id="myList">Action</h1>
+        <div class="box">
+          @foreach ($actionfilms->take(6) as $film)
+            <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}" class="h-48 w-48 mx-auto"></a>
+          @endforeach
+        </div>
+
+        <h1 id="myList">Horreur</h1>
+        <div class="box">
+          @foreach ($horrorfilms->take(6) as $film)
+            <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}" class="h-48 w-48 mx-auto"></a>
+          @endforeach
+        </div>
+
+        <h1 id="myList">Drame</h1>
+        <div class="box">
+          @foreach ($dramefilms->take(6) as $film)
+            <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}" class="h-48 w-48 mx-auto"></a>
+          @endforeach
+        </div>
+      @endrole
     </div>
-  </div>
-  @endrole
-
-  @role(1,3)
-  <div class="location" id="home"> <h1 id="home">Film pour enfant</h1> <div class="box">
-    @if (count($films))
-    @foreach ($filmsPG->take(6) as $film)
-    <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}"></a>
-    @endforeach
-
-    @else
-    <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/t1.PNG?raw=true" alt=""></a>
-    @endif
-    </div>
-  </div>
-  @endrole
-
-
-  @role(1,2)
-  <h1 id="myList">Action</h1>
-  <div class="box">
-    @foreach ($actionfilms->take(6) as $film)
-    <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}"></a>
-    @endforeach
-  </div>
-
-  <h1 id="myList">Horreur</h1>
-  <div class="box">
-    @foreach ($horrorfilms->take(6) as $film)
-    <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}"></a>
-    @endforeach
-  </div>
-
-
-  <h1 id="myList">Drame</h1>
-  <div class="box">
-    @foreach ($dramefilms->take(6) as $film)
-    <a href="{{ route('buggyflix.show', [$film]) }}"><img src="{{ asset('img/films/' . $film->pochette)}}" alt="" title="{{$film->pochette}}"></a>
-    @endforeach
-  </div>
-  @endrole
-  @else
-  <div class="main-container location" id="home">
-   <a class="location" href="{{ route('UsagersController.login')}}"><p>Veuillez vous connecter</p></a>
-</div>
   @endauth
   <!-- END OF MAIN CONTAINER -->
-  @endsection
+@endsection
