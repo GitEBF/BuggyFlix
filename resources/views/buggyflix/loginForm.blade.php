a53306ba8f1f4666d0f61696d4f32131e73ec45e
chumnon <lamothear02@gmail.com>
2023-12-04 13:45:19 -0500

    correction connexion

--- resources/views/buggyflix/loginForm.blade.php
+++ resources/views/buggyflix/loginForm.blade.php
@@ -17,14 +17,15 @@
             <input id="password" type="password" name="password"
                 class="block w-full px-4 py-2 mt-2 text-white-700 bg-white border border-gray-300 rounded-md bgFlixLight dark:text-gray-300 dark:border-gray-600 focus:border-red-500 dark:focus:border-red-500 focus:outline-none focus:ring ring-red-600">
         </div>
+        
     <div class="flex justify-end mt-6">
         <button
             class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-red-600 rounded-md hover:bg-red-800 focus:outline-none focus:bg-gray-600">Connexion</button>
-            <button class="padSignin px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-red-600 rounded-md hover:bg-red-800 focus:outline-none focus:bg-gray-600">
-                <a href="{{ route('UsagersController.showSignInForm')}}">Créer Compte</a>
-            </button>
-        </div>
-</form>
+            
+    </form>
+        <a href="{{ route('UsagersController.showSignInForm')}}" class="padSignin px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-red-600 rounded-md hover:bg-red-800 focus:outline-none focus:bg-gray-600">Créer Compte</a>
+    </div>
+
 <div class="flex justify-end mt-6">
 
 </div>
